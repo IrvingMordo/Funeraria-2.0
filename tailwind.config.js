8a5d88f9ebc40e6cb486e41e04588790bdb00816
IrvingM <irvonm92@gmail.com>
2023-12-09 13:29:13 -0600

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./views/**/*.pug'],
+  theme: {
+    extend: {
+      colors: {
+        "azulBonito": "#134074",
+        "naranja": "#FC9F5B",
+        "azulClarito": "#6A7B76", 
+        "verdesito" :"#588157"
+      }
+    },
+  },
+  plugins: [],
+}
+
